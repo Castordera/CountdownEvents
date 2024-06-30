116ee8c5648ff84f308c6747effba6698bc0a9cf
Ulises Castorena <u.castorenacaldera@olx.pl>
2024-06-30 21:52:21 +0200

    Add bundle for tests

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -8,10 +8,7 @@ dependencies {
     implementation(libs.javax.inject)
     implementation(libs.coroutines.core)
     //
-    testImplementation(libs.junit.jupiter)
-    testImplementation(libs.coroutines.test)
-    testImplementation(libs.mockk)
-    testImplementation(libs.turbine)
+    testImplementation(libs.bundles.unit.tests)
 }
 
 tasks.withType<Test> {
