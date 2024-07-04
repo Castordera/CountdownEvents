2e6696d62fb8f433d2f27a6bd77d7e917681ece6
Ulises Castorena <u.castorenacaldera@olx.pl>
2024-07-04 13:20:56 +0200

    clean imports in toolbar class

--- design/components/src/main/kotlin/com/ulises/components/toolbars/Toolbar.kt
+++ design/components/src/main/kotlin/com/ulises/components/toolbars/Toolbar.kt
@@ -3,12 +3,13 @@ package com.ulises.components.toolbars
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.ulises.components.R
 import com.ulises.theme.CountdownAppTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
