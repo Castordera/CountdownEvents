8f55caed9ca037e814d691e98068b1df70d0fd21
ulisescastorena <castor.ucc@gmail.com>
2025-05-28 18:01:59 -0600

    Refactor actions

--- feature/list/src/main/kotlin/com/ulises/list/models/Actions.kt
+++ feature/list/src/main/kotlin/com/ulises/list/models/Actions.kt
@@ -3,11 +3,17 @@ package com.ulises.list.models
 import com.example.domain.models.CountdownDate
 
 sealed interface Actions {
-    data object ToggleListType: Actions
-    data object DismissError: Actions
-    data class ChangeTimeCalculation(val item: CountdownDate): Actions
-    data class AddSelectedItem(val item: String): Actions
-    data object DeleteSelectedItems: Actions
-    data object CancelSelection: Actions
-    data class ChangeSelectedYear(val year: String): Actions
+    sealed interface Interaction: Actions {
+        data object ToggleListType : Interaction
+        data object DismissError : Interaction
+        data class ChangeTimeCalculation(val item: CountdownDate) : Interaction
+        data class AddSelectedItem(val item: String) : Interaction
+        data object DeleteSelectedItems : Interaction
+        data object CancelSelection : Interaction
+        data class ChangeSelectedYear(val year: String) : Interaction
+    }
+    sealed interface Navigation : Actions {
+        data object AddItem : Navigation
+        data class DetailItem(val item: CountdownDate) : Navigation
+    }
 }
